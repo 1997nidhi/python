fc7fbc3522e2987a2dc7bd61c135c86fc7eb2d0e
1997nidhi <74718182+1997nidhi@users.noreply.github.com>
2020-12-01 13:10:10 +0530

    updated

--- dice emulator.py
+++ dice emulator.py
@@ -38,42 +38,6 @@ x="y"
 rolling(x)
 
 
-# In[4]:
-
-
-from random import *
-while x=="y":
-    num=randint(1,6)
-    if num==1:
-            print("|       |")
-            print("|   *   |")
-            print("|       |")
-    if num==2:
-            print("| *     |")
-            print("|       |")
-            print("|     * |")
-    if num==3:
-            print("| *     |")
-            print("|   *   |")
-            print("|     * |")
-    if num==4:
-            print("| *   * |")
-            print("|       |")
-            print("| *   * |")
-    if num==5:
-            print("| *    * |")
-            print("|    *   |")
-            print("| *    * |")
-    if num==6:
-            print("| *   * |")
-            print("| *   * |")
-            print("| *   * |")
-    x=input("enter 'y' to continue the game or 'n' to exit")
-    print("\n")
-
-
-# In[ ]:
-
 
 
 
