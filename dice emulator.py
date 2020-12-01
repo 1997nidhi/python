00e1f6979723539de9ec24f75dd6b9da90266742
1997nidhi <74718182+1997nidhi@users.noreply.github.com>
2020-12-01 13:06:56 +0530

    dice emulator

---
+++ dice emulator.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+from random import *
+def rolling(x):
+    while x=="y":
+        num=randint(1,6)
+        if num==1:
+            print("|       |")
+            print("|   *   |")
+            print("|       |")
+        if num==2:
+            print("| *     |")
+            print("|       |")
+            print("|     * |")
+        if num==3:
+            print("| *     |")
+            print("|   *   |")
+            print("|     * |")
+        if num==4:
+            print("| *   * |")
+            print("|       |")
+            print("| *   * |")
+        if num==5:
+            print("| *    * |")
+            print("|    *   |")
+            print("| *    * |")
+        if num==6:
+            print("| *   * |")
+            print("| *   * |")
+            print("| *   * |")
+        x=input("enter 'y' to continue the game or 'n' to exit")
+        print("\n")
+x="y"
+rolling(x)
+
+
+# In[4]:
+
+
+from random import *
+while x=="y":
+    num=randint(1,6)
+    if num==1:
+            print("|       |")
+            print("|   *   |")
+            print("|       |")
+    if num==2:
+            print("| *     |")
+            print("|       |")
+            print("|     * |")
+    if num==3:
+            print("| *     |")
+            print("|   *   |")
+            print("|     * |")
+    if num==4:
+            print("| *   * |")
+            print("|       |")
+            print("| *   * |")
+    if num==5:
+            print("| *    * |")
+            print("|    *   |")
+            print("| *    * |")
+    if num==6:
+            print("| *   * |")
+            print("| *   * |")
+            print("| *   * |")
+    x=input("enter 'y' to continue the game or 'n' to exit")
+    print("\n")
+
+
+# In[ ]:
+
+
+
+
